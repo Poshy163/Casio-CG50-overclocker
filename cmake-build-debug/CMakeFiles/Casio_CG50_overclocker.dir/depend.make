9a0d8aa1612e8c70d3fc3bef6bf5944aa2ecb412
Joshua Leaper <leaperj@students.kbgs.sa.edu.au>
2021-07-07 20:10:24 +0930

    Small tweak

--- cmake-build-debug/CMakeFiles/Casio_CG50_overclocker.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Casio_CG50_overclocker.
-# This may be replaced when dependencies are built.
---
+++ projects/example/build/example.d
@@ -0,0 +1,6 @@
+example.o: \
+ C\:/GitHub/Repos/Casio-CG50-overclocker/projects/example/src/OverClocker.c \
+ C\:/GitHub/Repos/Casio-CG50-overclocker/include/fxcg/display.h \
+ C\:/GitHub/Repos/Casio-CG50-overclocker/include/fxcg/keyboard.h
+C\:/GitHub/Repos/Casio-CG50-overclocker/include/fxcg/display.h:
+C\:/GitHub/Repos/Casio-CG50-overclocker/include/fxcg/keyboard.h:
