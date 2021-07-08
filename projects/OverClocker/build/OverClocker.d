d3d964dbeed901c69d521a79bbcec749e91ca9c2
Joshua Leaper <leaperj@students.kbgs.sa.edu.au>
2021-07-08 21:48:03 +0930

    Updated file names to better reflect the program

--- projects/OverClocker/build/OverClocker.d
+++ projects/OverClocker/build/OverClocker.d
@@ -1,5 +1,5 @@
-example.o: \
- C\:/GitHub/Repos/Casio-CG50-overclocker/projects/example/src/OverClocker.c \
+OverClocker.o: \
+ C\:/GitHub/Repos/Casio-CG50-overclocker/projects/OverClocker/src/OverClocker.c \
  C\:/GitHub/Repos/Casio-CG50-overclocker/include/fxcg/display.h \
  C\:/GitHub/Repos/Casio-CG50-overclocker/include/fxcg/keyboard.h
 C\:/GitHub/Repos/Casio-CG50-overclocker/include/fxcg/display.h:
